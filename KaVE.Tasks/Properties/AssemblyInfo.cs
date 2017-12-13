a7bd384dd54135db714c81bd50249b32faff789e
Nico Strebel <ncstrbl@gmail.com>
2017-12-13 14:47:11 +0100

    fix typo in assemblyinfo

--- KaVE.Tasks/Properties/AssemblyInfo.cs
+++ KaVE.Tasks/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("KaVE.Tasks.Test")]
+[assembly: InternalsVisibleTo("KaVE.Tasks.Tests")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4304740e-cebd-4584-9e13-6c5dffdd8a71")]
