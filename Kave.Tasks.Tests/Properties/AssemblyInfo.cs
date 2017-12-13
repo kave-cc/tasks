32e4f98c86b08b167588fc96106907895a7000d0
Nico Strebel <ncstrbl@gmail.com>
2017-12-13 14:39:22 +0100

    fix typo in test assembly name

--- Kave.Tasks.Tests/Properties/AssemblyInfo.cs
+++ Kave.Tasks.Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("KaVE.Tasks.Test")]
+[assembly: AssemblyTitle("KaVE.Tasks.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KaVE.Tasks.Test")]
+[assembly: AssemblyProduct("KaVE.Tasks.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
